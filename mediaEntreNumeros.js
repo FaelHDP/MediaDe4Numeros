4fa25a44b8a4aada311d089467c2e0be6e0e7d1d
FaelHDP <69848490+FaelHDP@users.noreply.github.com>
2020-10-31 16:37:30 -0300

    Add files via upload

---
+++ mediaEntreNumeros.js
@@ -0,0 +1,7 @@
+let a = parseFloat(prompt('Digite o primeiro algarismo: '))
+let b = parseFloat(prompt('Digite o segundo algarismo: '))
+let c = parseFloat(prompt('Digite o terceiro algarismo: '))
+let d = parseFloat(prompt('Digite o quarto algarismo: '))
+let media = (a + b + c) / 3
+
+prompt(`Media dos algarismos digitados: ${media}`)
